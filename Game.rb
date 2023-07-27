20b64a85e0884cc4d2f7784498063027f40bb7ba
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-27 00:04:46 +0000

    added question basic

--- Game.rb
+++ Game.rb
@@ -13,9 +13,16 @@ class Game
 
   def round_increment! #exclamation for mutable
     @current_round += 1
+    puts "current_round"
   end
 
-  
+  def next_player!(current_player)
+    puts "Switch player(next_player)"
+  end
+
+  def end_game
+    puts "game finished(end_game)"
+  end
 
 
 end
--- main.rb
+++ main.rb
@@ -2,13 +2,12 @@ require './Game'
 require './Player'
 require './Question'
 
-# game = Game.new
+game = Game.new
 # game.start_game
 
 
 player1 = Player.new('Alex')
-player1.name
-player1.lives
-player1.lose_life
-player1.lose_life
-player1.lose_life
+
+question = Question.new
+puts question.show_question
+
