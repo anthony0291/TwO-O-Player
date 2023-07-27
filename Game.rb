adb51af4f4ec26bdd595c0038c7b31c484225550
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-07-27 01:54:01 +0000

    finished assignment

--- Game.rb
+++ Game.rb
@@ -1,52 +1,63 @@
 class Game
-  attr_accessor :current_round, :current_player, :answer, :run_round, :start_question
+  attr_accessor :current_round, :current_player
 
   def initialize 
-    @current_player = @player_one
-    @current_round = 0
+    @current_player = nil
     @players = []
-    @answer = nil
+    @current_round = 0
   end
 
   def run_game
     puts "Racers start your engines!"
+    player_one = Player.new
+    player_two = Player.new
+    player_one.name = "Player 1"
+    player_two.name = "Player 2"
+    @players << player_one
+    @players << player_two
+    @current_player = player_two
+    
+
     run_round
   end
 
-  def run_round
-    while @current_player(:lives) > 0 do
-    round_increment!
-    next_player!
-    puts "Round: #{@current_round}! FIGHT!"
-    answer = $stdin.gets.chomp.to_i
-      if question.is_correct?(answer)
-        puts "#{@current_player}: Yes! You are correct."
-      else
-        puts "#{@current_player}: Seriously? No!"
-      end
-    puts "P1: #{@player_one.lives}/3 P2: #{@player_two.lives}/3"
-    end
-  end
 
   def round_increment! #exclamation for mutable
     @current_round += 1
-    puts "current_round"
   end
 
-  def next_player!(current_player)
-    if @current_player == @player_one
-      @current_player = @player_two
-    elsif @current_player = @player_two
-      @current_player = @player_one
+  def next_player!
+    if (@current_player == @players[0])
+      @current_player = @players[1]
+    elsif (@current_player == @players[1])
+      @current_player = @players[0]
     end
-  
   end
 
   def end_game
     puts "***************GAME OVER *******************"
-    puts "#{@current_player} you have zero lives."
+    puts "#{@current_player.name} you have zero lives."
     next_player!
-    puts "#{@current_player} you have #{current_player.lives} left and are the Greetest!" 
+    puts "#{@current_player.name} you have #{@current_player.lives} left and are the Greetest!" 
   end
 
+  def run_round
+    while (@current_player.lives > 0) do
+      round_increment!
+      next_player!
+      question = Question.new
+      puts "Question: #{@current_round}"
+      puts "#{@current_player.name}: What is #{question.show_question}"
+      answer = $stdin.gets.chomp.to_i
+      if question.is_correct?(answer)
+        puts "#{@current_player.name}: Yes! You are correct!"
+      else
+        puts "#{@current_player.name}: Seriously? No!"
+        @current_player.lose_life
+      end
+      puts "P1: #{@players[0].lives}/3 P2: #{@players[1].lives}/3"
+      puts "_____NEW TURN_____"
+    end
+      end_game
+  end
 end
--- Question.rb
+++ Question.rb
@@ -1,29 +1,19 @@
 class Question
-  attr_accessor :show_question, :answer, :correct_answer
+  attr_accessor :correct_answer
 
   def initialize
     @number_one = rand(1..20)
     @number_two = rand(1..20)
-    @correct_answer = (@number_one + @number_two)
+    @correct_answer = @number_one + @number_two
   end
 
   def show_question
-    puts "What is #{@number_one} + #{@number_two}? "
+    "#{@number_one} + #{@number_two}"
   end
 
-  def get_answer
-    answer = $stdin.gets.chomp
-    puts "> #{answer}"
-    is_correct?(answer)
+  def is_correct?(answer)
+    answer == @correct_answer
   end
-
-  def is_correct?(guess)
-    puts @correct_answer
-    if guess.to_i == @correct_answer
-      puts "Good Job!"
-    else
-      puts "Sorry that's not right!"
-
-    end
-  end
+  
 end
+
